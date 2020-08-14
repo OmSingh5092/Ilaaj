f52c5281bac9fccb960509539a6a88888332770a
OmSingh <omsingh6903@gmail.com>
2020-08-14 12:06:21 +0530

    Added retrofit

---
+++ app/src/main/java/com/codenite/ilaaj/retrofit/APIInterface.java
@@ -0,0 +1,24 @@
+package com.codenite.ilaaj.retrofit;
+
+
+
+import java.util.Map;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.HTTP;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface APIInterface {
+
+
+
+}
