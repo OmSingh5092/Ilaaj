facfeface39821cda915e6e6049c9ae9b1a2863d
OmSingh <omsingh6903@gmail.com>
2020-08-24 12:01:25 +0530

    Added fragments to the dashboard

--- app/src/main/java/com/codenite/ilaaj/activities/HomeActivity.java
+++ app/src/main/java/com/codenite/ilaaj/activities/HomeActivity.java
@@ -2,21 +2,72 @@ package com.codenite.ilaaj.activities;
 
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MenuItem;
 
+import com.codenite.ilaaj.R;
 import com.codenite.ilaaj.databinding.ActivityHomeBinding;
+import com.codenite.ilaaj.fragments.DashboardFragment;
+import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class HomeActivity extends AppCompatActivity {
     ActivityHomeBinding binding;
+    FragmentTransaction ft;
+    Fragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
+        //Setting up fragment fragment transaction
+        ft = getSupportFragmentManager().beginTransaction();
 
+        setUpSideNav();
 
+    }
+
+    private void setUpSideNav(){
+        addFragment(0);
+        binding.sideNav.navigation1.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.make_appointment){
+
+                }else if(item.getItemId() == R.id.conversation){
+
+                }else if(item.getItemId()== R.id.manage_documents){
+
+                }
+                return false;
+            }
+        });
+
+        binding.sideNav.navigation2.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.profile){
+
+                }else if(item.getItemId() == R.id.logout){
+
+                }
+                return false;
+            }
+        });
+    }
+
+    void addFragment(int ind){
+
+        switch (ind){
+            case 0:
+                fragment = new DashboardFragment();
+                ft.add(binding.fragmentFrame.getId(),fragment).commit();
+                break;
+        }
     }
 
     @Override
--- app/src/main/java/com/codenite/ilaaj/fragments/DashboardFragment.java
+++ app/src/main/java/com/codenite/ilaaj/fragments/DashboardFragment.java
@@ -5,15 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.codenite.ilaaj.R;
+import com.codenite.ilaaj.databinding.FragmentDashboardBinding;
 
 import androidx.fragment.app.Fragment;
 public class DashboardFragment extends Fragment {
-
+    FragmentDashboardBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        binding = FragmentDashboardBinding.inflate(LayoutInflater.from(getActivity()),container,false);
+        return binding.getRoot();
     }
 }
