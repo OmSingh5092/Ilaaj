484a226b74419c575894471b0eed187947d54a60
OmSingh <omsingh6903@gmail.com>
2020-08-27 23:43:11 +0530

    Made appointment model

--- app/src/main/java/com/codenite/ilaaj/api/dataModels/Appointment.java
+++ app/src/main/java/com/codenite/ilaaj/api/dataModels/Appointment.java
@@ -4,9 +4,74 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class Appointment {
-    @SerializedName("appointment_id")
+    @SerializedName("id")
     @Expose
     int appointmentId;
+    @SerializedName("user_id")
+    @Expose
+    int userId;
+    @SerializedName("dateTime")
+    @Expose
+    String dateTime;
+    @SerializedName("payment_complete")
+    @Expose
+    boolean paymentComplete;
+    @SerializedName("meet_link")
+    @Expose
+    String meetLink;
+    @SerializedName("createdAt")
+    @Expose
+    String createdAt;
+    @SerializedName("updatedAt")
+    String updatedAt;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public boolean isPaymentComplete() {
+        return paymentComplete;
+    }
+
+    public void setPaymentComplete(boolean paymentComplete) {
+        this.paymentComplete = paymentComplete;
+    }
+
+    public String getMeetLink() {
+        return meetLink;
+    }
+
+    public void setMeetLink(String meetLink) {
+        this.meetLink = meetLink;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 
     public int getAppointmentId() {
         return appointmentId;
