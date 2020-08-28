4a1aca5e9bf84cbfcec96db1672b29a8df11a9d9
OmSingh <omsingh6903@gmail.com>
2020-08-29 00:53:26 +0530

    Made payment fragment

--- app/src/main/java/com/codenite/ilaaj/fragments/PaymentFragment.java
+++ app/src/main/java/com/codenite/ilaaj/fragments/PaymentFragment.java
@@ -1,18 +1,74 @@
 package com.codenite.ilaaj.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.codenite.ilaaj.api.controllers.UserController;
+import com.codenite.ilaaj.api.dataModels.User;
+import com.codenite.ilaaj.databinding.FragmentPaymentBinding;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class PaymentFragment extends Fragment {
+    FragmentPaymentBinding binding;
+    String upi, charge;
+    Context context;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        binding = FragmentPaymentBinding.inflate(getLayoutInflater(),container,false);
+        context = getContext();
+
+        binding.submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getText();
+                if(!validate()) {
+                    return;
+                }
+                update();
+
+            }
+        });
+        return binding.getRoot();
     }
+
+    private void getText(){
+        upi = binding.upiId.getText().toString();
+        charge  = binding.charge.getText().toString();
+    }
+
+    private void update(){
+        new UserController(context).getUser(new UserController.userGetHandler() {
+            @Override
+            public void onSuccess(User user) {
+                //Set new values
+            }
+
+            @Override
+            public void onFailure(Throwable e) {
+
+            }
+        });
+    }
+
+    private boolean validate(){
+        if(charge == null){
+            Toast.makeText(context, "Please enter a price", Toast.LENGTH_SHORT).show();
+            return false;
+        }else if(upi == null){
+            Toast.makeText(context, "Please enter an UPI ID", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+
 }
